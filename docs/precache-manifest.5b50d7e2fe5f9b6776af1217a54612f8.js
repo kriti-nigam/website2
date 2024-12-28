9a3f6e08843ca83dad5a47a6a009f446a702cc46
Kriti Nigam <kritinigam3299@gmail.com>
2024-12-28 23:15:14 +0530

    Bash Script Commit

---
+++ docs/precache-manifest.5b50d7e2fe5f9b6776af1217a54612f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf1736a194989b0b6c7ca9331a6495b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97bcd23e0e0035d66df5",
+    "url": "/static/css/main.a073dc6c.chunk.css"
+  },
+  {
+    "revision": "00b54171d806e6131500",
+    "url": "/static/js/2.4b8b797a.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/static/js/2.4b8b797a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97bcd23e0e0035d66df5",
+    "url": "/static/js/main.d07a6c13.chunk.js"
+  },
+  {
+    "revision": "1eb29b79da48b124f7b5",
+    "url": "/static/js/runtime-main.8ad5dc57.js"
+  }
+]);
